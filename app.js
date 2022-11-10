264535ad65ffa4af1321a9dab5eeecdd8bd8dfef
webmasterBotike <116920137+webmasterBotike@users.noreply.github.com>
2022-11-10 14:50:35 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+
+require('dotenv').config();
+console.log(process.env.GOOGLE_MAPS_API);
+
+var falaUser = require("./routes/falaUser");
+
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use("/falauser", falaUser);
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.listen(4682, () => {
+    console.log('Rodando fino na porta 4682');
+});
